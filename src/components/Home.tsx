29eb4e6222636708dc1186c498c803328ffa0323
PratyakshG <pincodeazm@gmail.com>
2024-07-13 20:09:35 +0530

    created clock and display quotes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,49 +1,193 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { FaShareAlt } from "react-icons/fa";
 
 function Home() {
+  const [quote, setQuote] = useState<string>("");
+
+  // speed factor to control the countdown pace
   const [speed, setSpeed] = useState("1");
 
+  //boolean to start the countdown function
+  const [startCountdown, setStartCountdown] = useState<boolean>(false);
+
+  //provides the current time
+  const [currentTime, setCurrentTime] = useState<Date>(new Date());
+
+  //provides the time to when the clock should move during countdown
+  const [targetTime, setTargetTime] = useState<Date>(new Date());
+
+  // function to fetch random quotes
+  useEffect(() => {
+    const getQuotes = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.api-ninjas.com/v1/quotes",
+          {
+            headers: {
+              "X-Api-Key": "NMJlU5iFdYgKLUScW1SsFA==22dQrxaCAcsvNafa",
+            },
+          },
+        );
+        // console.log(response);
+        setQuote(response?.data[0].quote);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getQuotes();
+
+    const updateQuotes = setInterval(getQuotes, 5000);
+
+    return () => {
+      clearInterval(updateQuotes);
+    };
+  }, []);
+
+  // function to get the current time and target time for the countdown
+  useEffect(() => {
+    //get time from search parameters of the shared url
+
+    const updateTime = setInterval(() => {
+      if (startCountdown) {
+        setCurrentTime((prevTime) => {
+          const newTime = new Date(prevTime.getTime() - 1000);
+          if (newTime <= targetTime) {
+            setStartCountdown(false);
+          }
+          return newTime;
+        });
+      } else {
+        setCurrentTime(new Date());
+        setTargetTime(new Date(new Date().getTime() - 120 * 60 * 1000));
+      }
+    }, 1000 / parseInt(speed));
+
+    return () => {
+      clearInterval(updateTime);
+    };
+  }, [startCountdown, targetTime, speed]);
+
+  const hourDeg =
+    ((currentTime.getHours() % 12) + currentTime.getMinutes() / 60) * 30;
+
+  const minDeg =
+    ((currentTime.getMinutes() + currentTime.getSeconds() / 60) * 6) % 360;
+
+  const secDeg =
+    ((currentTime.getSeconds() + currentTime.getMilliseconds() / 1000) * 6) %
+    360;
+
+  const handleShare = () => {
+    // const url = new URL(window.location.href);
+    const currentTimeParam = currentTime.getTime();
+    const targetTimeParam = targetTime.getTime();
+
+    // url.searchParams.set("currentTime", currentTimeParam.toLocaleString());
+    // url.searchParams.set("targetTime", targetTimeParam.toLocaleString());
+    // url.searchParams.set("speed");
+
+    console.log(window.location.href);
+
+    navigator.share({
+      url: `${window.location.href}?currentTime=${currentTimeParam}&targetTime=${targetTimeParam}`,
+    });
+  };
+
   return (
-    <div className="h-dvh w-dvw flex flex-col items-center gap-8 px-6">
+    <div className="mt-[76px] max-h-dvh w-dvw flex flex-col items-center gap-5 px-6 drop-shadow-md">
+      <div className="flex w-full items-center justify-between gap-5">
+        {/* Target Time */}
+        <div className="border-2 border-primary px-4 py-2 rounded-lg w-full">
+          <div className="font-semibold">Target Time</div>
+          <div className="text-sm">{targetTime.toLocaleTimeString()}</div>
+        </div>
+
+        {/* Current Time */}
+        <div className="text-right border-2 border-primary px-4 py-2 rounded-lg w-full">
+          <div className="font-semibold">Current Time</div>
+          <div className="text-sm">{currentTime.toLocaleTimeString()}</div>
+        </div>
+      </div>
+
       {/* Clock */}
-      <div className="mt-[76px] relative h-80 w-80 border-[10px] border-black-1 bg-primary rounded-full">
+      <div className="relative h-80 w-80 border-[12px] border-primary bg-black-1 rounded-full">
         {/* second hand */}
-        <div className="bg-black-1 absolute h-[10px] w-[140px] rounded-full -translate-y-[5px] left-1/2 top-1/2 origin-bottom-left" />
+        <div
+          style={{
+            rotate: `${secDeg}deg`,
+          }}
+          className={`bg-primary absolute h-[135px] w-[4px] rounded-full -translate-x-[2px] left-1/2 bottom-1/2 origin-bottom-left z-30`}
+        />
 
         {/* minute hand */}
-        <div className="bg-gray-600 absolute h-[10px] w-[120px] rounded-full -translate-y-[5px] left-1/2 top-1/2 origin-bottom-left" />
+        <div
+          style={{
+            rotate: `${minDeg}deg`,
+          }}
+          className="bg-white absolute h-[120px] w-[10px] rounded-full -translate-x-[5px] left-1/2 bottom-1/2 origin-bottom-left z-10"
+        />
 
         {/* hour hand */}
-        <div className="bg-white absolute h-[10px] w-[100px] rounded-full -translate-y-[5px] left-1/2 top-1/2 origin-bottom-left" />
+        <div
+          style={{
+            rotate: `${hourDeg}deg`,
+          }}
+          className="bg-gray-1 border-x-4 border-t-4 border-white absolute h-[90px] w-[10px] rounded-full -translate-x-[5px] left-1/2 bottom-1/2 origin-bottom-left z-20"
+        />
 
         {/* center dot */}
-        <div className="bg-gray-1 border-2 border-black-1 absolute h-[20px] w-[20px] rounded-full -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 origin-bottom-left" />
+        <div className="bg-white absolute h-[20px] w-[20px] rounded-full -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 origin-bottom-left z-30" />
+        <div className="bg-primary absolute h-[10px] w-[10px] rounded-full -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 origin-bottom-left z-30" />
       </div>
 
-      {/* Slider */}
-      <div className="flex flex-col gap-2">
-        <input
-          type="range"
-          min="1"
-          max="5"
-          onChange={(e) => {
-            setSpeed(e.target.value);
-          }}
-        />
-        <label>Slider Speed : {speed}x</label>
-      </div>
+      <div className="w-full flex items-center justify-between">
+        {/* Slider */}
+        <div className="flex flex-col gap-2 items-center justify-center">
+          <input
+            type="range"
+            min="1"
+            max="10"
+            defaultValue="1"
+            disabled={startCountdown ? false : true}
+            onChange={(e) => {
+              setSpeed(e.target.value);
+            }}
+            className="appearance-none bg-black-1 rounded-full h-2 disabled:opacity-50"
+          />
+          <label className="font-medium text-gray-1">
+            Slider Speed : {speed}x
+          </label>
+        </div>
 
-      {/* Quotes */}
-      <div>Quotes</div>
+        {/* countdown button */}
+        <div>
+          <button
+            onClick={() => {
+              setStartCountdown(true);
+              console.log(startCountdown);
+            }}
+            className="w-full bg-primary px-6 py-4 rounded-full font-semibold text-white text-sm"
+          >
+            Start Countdown
+          </button>
+        </div>
+      </div>
 
       {/* Share Button */}
       <div className="w-full">
         <button
-          onClick={() => {}}
-          className="w-full bg-primary py-4 rounded-full font-semibold text-white text-sm"
+          onClick={handleShare}
+          className="w-full flex items-center justify-center gap-2 bg-primary py-4 rounded-full font-semibold text-white"
         >
-          Go to Tracking Screen
+          Share URL <FaShareAlt size={16} />
         </button>
+      </div>
+
+      {/* Quotes */}
+      <div className="border-2 border-gray-1 p-2 rounded-lg text-sm font-medium max-h-60 overflow-y-scroll">
+        "{quote}"
       </div>
     </div>
   );
