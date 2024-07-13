c1f080989ed9611e67d1f1f8b8de87260c89c587
PratyakshG <pincodeazm@gmail.com>
2024-07-13 20:17:30 +0530

    removed AuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,31 +1,31 @@
-import { createContext, useState, useEffect } from "react";
-import { auth } from "../firebase-config";
+// import { createContext, useState, useEffect } from "react";
+// import { auth } from "../firebase-config";
 
-interface AuthContextType {
-  currentUser: firebase.User | null;
-}
+// interface AuthContextType {
+//   currentUser: firebase.User | null;
+// }
 
-const AuthContext = createContext<AuthContextType>({
-  currentUser: null,
-});
+// const AuthContext = createContext<AuthContextType>({
+//   currentUser: null,
+// });
 
-const AuthProvider = ({ children }: any) => {
-  const [currentUser, setCurrentUser] = useState<firebase.User | null>(null);
+// const AuthProvider = ({ children }: any) => {
+//   const [currentUser, setCurrentUser] = useState<firebase.User | null>(null);
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      setCurrentUser(user);
-    });
+//   useEffect(() => {
+//     const unsubscribe = auth.onAuthStateChanged((user) => {
+//       setCurrentUser(user);
+//     });
 
-    return () => unsubscribe();
-  }, []);
+//     return () => unsubscribe();
+//   }, []);
 
-  return (
-    <AuthContext.Provider value={{ currentUser }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+//   return (
+//     <AuthContext.Provider value={{ currentUser }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
 
-export default AuthContext;
-export { AuthProvider };
+// export default AuthContext;
+// export { AuthProvider };
