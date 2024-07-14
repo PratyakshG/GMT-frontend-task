7ab9e2ebd9d06bb8b0ce2b3a04b5b29364bf8d72
PratyakshG <pincodeazm@gmail.com>
2024-07-14 16:46:39 +0530

    updated query string configuration to only send speed

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,27 +7,18 @@ function Home() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [quote, setQuote] = useState<string>("");
   const speedParam = searchParams.get("speed") || "1";
-  const countdownParam = searchParams.get("countdown") === "true";
 
   // speed factor to control the countdown pace
   const [speed, setSpeed] = useState(speedParam);
 
   //boolean to start the countdown function
-  const [startCountdown, setStartCountdown] = useState<boolean>(countdownParam);
+  const [startCountdown, setStartCountdown] = useState<boolean>(false);
 
   //provides the current time
-  const [currentTime, setCurrentTime] = useState<Date>(
-    searchParams.get("currentTime")
-      ? new Date(parseInt(searchParams.get("currentTime") || "0"))
-      : new Date(),
-  );
+  const [currentTime, setCurrentTime] = useState<Date>(new Date());
 
   //provides the time to when the clock should move during countdown
-  const [targetTime, setTargetTime] = useState<Date>(
-    searchParams.get("targetTime")
-      ? new Date(parseInt(searchParams.get("targetTime") || "0"))
-      : new Date(new Date().getTime() - 120 * 60 * 1000),
-  );
+  const [targetTime, setTargetTime] = useState<Date>(new Date());
   // function to fetch random quotes
   useEffect(() => {
     const getQuotes = async () => {
@@ -83,12 +74,9 @@ function Home() {
   //update query parameters
   useEffect(() => {
     setSearchParams({
-      currentTime: currentTime.getTime().toString(),
-      targetTime: targetTime.getTime().toString(),
       speed: speed,
-      countdown: startCountdown.toString(),
     });
-  }, [currentTime, targetTime, speed, startCountdown, setSearchParams]);
+  }, [speed, setSearchParams]);
 
   const hourDeg =
     ((currentTime.getHours() % 12) + currentTime.getMinutes() / 60) * 30;
@@ -104,7 +92,7 @@ function Home() {
     console.log(window.location.href);
 
     navigator.share({
-      url: `${window.location.href}?currentTime=${currentTime}&targetTime=${targetTime}&speed=${speed}&countdown=${startCountdown}`,
+      url: `${window.location.href}?speed=${speed}`,
     });
   };
 
@@ -165,7 +153,6 @@ function Home() {
             min="1"
             max="10"
             defaultValue="1"
-            disabled={startCountdown ? false : true}
             onChange={(e) => {
               setSpeed(e.target.value);
             }}
